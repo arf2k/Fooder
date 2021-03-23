061da5323df098152684f2e9c3684e4cd7295131
https://learn.co/arf2k <foreman.alexander@gmail.com>
2021-03-23 15:10:04 -0400

    Another test

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import SearchScreen from "./src/screens/SearchScreen"
+require("dotenv").config()
 
 const navigator = createStackNavigator({
   Search: SearchScreen, 
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,19 +1,22 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
-import SearchBar from "../components/SearchBar"
+import SearchBar from "../components/SearchBar";
 
 const SearchScreen = () => {
+  const [term, setTerm] = useState("");
 
-     const [term, setTerm] = useState("")
+  return (
+    <View>
+      <SearchBar
+        term={term}
+        onTermChange={(newTerm) => setTerm(newTerm)}
+        onTermSubmit={() => console.log("term submitted")}
+      />
+      <Text> Search Screen </Text>
+    </View>
+  );
+};
 
-     return (
-          <View>
-               <SearchBar term={term} onTermChange={newTerm => setTerm(newTerm)} onTermSubmit={()=> console.log("term submitted")}/>
-               <Text> Search Screen </Text>
-          </View>
-     )
-}
+const styles = StyleSheet.create({});
 
-const styles = StyleSheet.create({})
-
-export default SearchScreen
+export default SearchScreen;
